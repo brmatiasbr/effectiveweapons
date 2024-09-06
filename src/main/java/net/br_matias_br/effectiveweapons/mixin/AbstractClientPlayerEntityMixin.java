8101f0c8046731002d8cd584c3ea8583ca720c49
brmatiasbr <matiasig2004@gmail.com>
2024-09-05 23:40:44 -0300

    Fixed spectator mode being broken

--- src/main/java/net/br_matias_br/effectiveweapons/mixin/AbstractClientPlayerEntityMixin.java
+++ src/main/java/net/br_matias_br/effectiveweapons/mixin/AbstractClientPlayerEntityMixin.java
@@ -6,19 +6,24 @@ import net.br_matias_br.effectiveweapons.item.EffectiveWeaponsItems;
 import net.br_matias_br.effectiveweapons.item.custom.DoubleBowItem;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.AbstractClientPlayerEntity;
+import net.minecraft.client.network.PlayerListEntry;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.GameMode;
 import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(AbstractClientPlayerEntity.class)
 public class AbstractClientPlayerEntityMixin extends PlayerEntity {
+    @Shadow private @Nullable PlayerListEntry playerListEntry;
+
     public AbstractClientPlayerEntityMixin(World world, BlockPos pos, float yaw, GameProfile gameProfile) {
         super(world, pos, yaw, gameProfile);
     }
@@ -37,11 +42,23 @@ public class AbstractClientPlayerEntityMixin extends PlayerEntity {
 
     @Override
     public boolean isSpectator() {
-        return false;
+        PlayerListEntry playerListEntry = this.getPlayerListEntry();
+        return playerListEntry != null && playerListEntry.getGameMode() == GameMode.SPECTATOR;
     }
 
     @Override
     public boolean isCreative() {
-        return false;
+        PlayerListEntry playerListEntry = this.getPlayerListEntry();
+        return playerListEntry != null && playerListEntry.getGameMode() == GameMode.CREATIVE;
+    }
+
+    @Unique
+    @Nullable
+    protected PlayerListEntry getPlayerListEntry() {
+        if (this.playerListEntry == null) {
+            this.playerListEntry = MinecraftClient.getInstance().getNetworkHandler().getPlayerListEntry(this.getUuid());
+        }
+
+        return this.playerListEntry;
     }
 }
